1a956eaf256b2bdeea8bf1a8b92845545a28f635
Eduardo Aleixo <eduardo@carpa.dev>
2025-09-02 20:33:43 +0100

    finish footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 import styles from "./Footer.module.css";
+import CarpaBubblesImg from "./carpa_cute.svg";
+import Image from "next/image";
 
 export function Footer() {
   return (
     <footer>
       <div className={styles.waves}></div>
       <div className={styles.footer}>
-        <div className="container">
-          <Link href="https://www.svgbackgrounds.com/license/" target="_blank">
-            SVG License
-          </Link>
+        <div className="container flex justify-center">
+          <Image
+            className={styles.carpaBubbles}
+            alt="Carpa nadando"
+            src={CarpaBubblesImg}
+            width="100"
+            height="100"
+          />
+
+          <div className="flex items-center">
+            Carpa.dev {new Date().getFullYear()} Todos os direitos reservados
+          </div>
         </div>
       </div>
     </footer>
