10f8bcedb480b86270b40fe6dd87576a00accef6
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2025-09-02 14:22:09 -0300

    replace icons for svg

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "./logo.png";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const links = [
   {
@@ -56,7 +55,9 @@ export function TopBar() {
         onClick={() => setIsOpen(true)}
         aria-label="Abrir menu"
       >
-        <Bars3Icon className="size-6" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+          <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+        </svg>
       </button>
 
       <div
@@ -74,7 +75,9 @@ export function TopBar() {
           <div className="flex justify-between items-center p-4">
             <div></div>
             <button onClick={() => setIsOpen(false)} aria-label="Fechar menu">
-              <XMarkIcon className="size-6" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+              </svg>
             </button>
           </div>
 
